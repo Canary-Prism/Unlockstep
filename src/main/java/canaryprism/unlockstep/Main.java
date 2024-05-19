3e874671111ab7da8f0d26e35951e2b2f6b41ea9
Canary-Prism <canaryprsn@gmail.com>
2024-05-19 16:26:17 +0800

    changed structure for includsion of lockstep 2

--- src/main/java/canaryprism/unlockstep/Main.java
+++ src/main/java/canaryprism/unlockstep/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        var lockstep = new Lockstep("/unlockstep_assets");
+        var lockstep = new Lockstep("/unlockstep_assets/lockstep1");
 
         lockstep.start().join();
         
